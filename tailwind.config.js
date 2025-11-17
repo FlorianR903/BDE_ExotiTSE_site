bf4fbe8c34f65a41ce948c60317fd89670537902
florian.rmgs <florian.raman09@gmail.com>
2025-11-18 00:31:17 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+theme: {
+extend: {
+colors: {
+tropical1: '#FFD166',
+tropical2: '#F76BAA',
+tropical3: '#8E44AD'
+}
+},
+},
+plugins: [],
+}
