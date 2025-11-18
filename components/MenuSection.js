deffe5692c0fc5bfb68896c1065c616d58b367fe
Florian Ramangasalama <florian.raman09@gmail.com>
2025-11-18 20:42:33 +0100

    Création du component MenuSection.js

---
+++ components/MenuSection.js
@@ -0,0 +1,9 @@
+// components/MenuSection.js
+export default function MenuSection() {
+    return (
+        <section className="py-20 text-white text-center">
+            <h2 className="text-4xl font-bold mb-6">Menu</h2>
+            <p>Ajoute ici ton contenu du menu</p>
+        </section>
+    );
+}
