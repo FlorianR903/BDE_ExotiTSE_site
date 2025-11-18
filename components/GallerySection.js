297cce0d71f7a2a0e0faf61ebb1cc934ef75d850
Florian Ramangasalama <florian.raman09@gmail.com>
2025-11-18 20:40:51 +0100

    Création du component GallerySection.js

---
+++ components/GallerySection.js
@@ -0,0 +1,9 @@
+// components/GallerySection.js
+export default function GallerySection() {
+    return (
+        <section className="py-20 text-white text-center">
+            <h2 className="text-4xl font-bold mb-6">Galerie</h2>
+            <p>Ajoute ici ta galerie d’images</p>
+        </section>
+    );
+}
