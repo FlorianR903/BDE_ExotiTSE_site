8dfe2374337ad8d60fd684cc6a787b6c3cf4616e
Florian <florian.raman09@gmail.com>
2026-01-28 17:54:11 +0100

    test

--- components/EventsSection.js
+++ components/EventsSection.js
@@ -27,7 +27,7 @@ export default function EventsSection({ events = [] }) {
             title: "Le fameux Apparthon 🍻🏃‍♂️",
             date: "28 janvier 2026 à partir de 20h",
             desc: "Pourquoi rester dans un seul bar quand on peut explorer bien au-délà ? Ce soir, on transforme nos salons en escales festives 🎉 ! L'Apparthon, c'est le marathon ultime où la ligne d'arrivée n'est qu'un prétexte pour faire la fête !",
-            details: "💥 Au programme :" + "\n Prenez part à 6 soirées dans Sainté ! " + "Dans chaque lieu : 1 thème - 1 activité - 1 nouvelle ambiance 🔥 ! " + " N'oubliez pas de récupérer votre carte de passage au 1er appart ! " + "🚨 1 livraison gratuite est offerte aux personnes ayant récolté TOUS les stickers !"
+            details: "💥 Au programme :" + "\n Prenez part à 6 soirées dans Sainté ! " + "Dans chaque lieu : 1 cocktail - 1 activité - 1 nouvelle ambiance 🔥 ! " + " N'oubliez pas de récupérer votre carte de passage au 1er appart ! " + "🚨 1 réduction de -50% est offerte aux personnes ayant traversé TOUS les apparts !"
         },
         {
             id: 4,
